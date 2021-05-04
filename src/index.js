6ef0245cea879ec2a24cd1cc6ec0d16c26dc5cf4
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2021-05-05 01:10:23 +0300

    New: disable import rules for tests folder

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable more/no-hardcoded-password */
 /* eslint-disable no-magic-numbers */
 module.exports = {
     'parser' : 'babel-eslint',
@@ -274,7 +275,10 @@ module.exports = {
             'rules' : {
                 'prefer-arrow-callback'              : 0,
                 'security/detect-non-literal-regexp' : 0,
-                'no-magic-numbers'                   : 0
+                'no-magic-numbers'                   : 0,
+                'more/no-hardcoded-password'         : 0,
+                'import/named'                       : 0,
+                'import/default'                     : 0
             }
         }
     ]
