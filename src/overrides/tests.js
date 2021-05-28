3da7c9955dc0fb851ebfe180ae0993cfbcbe99f3
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2021-05-28 21:22:44 +0300

    Fix: allow camelCase file name for tests

--- src/overrides/tests.js
+++ src/overrides/tests.js
@@ -22,7 +22,8 @@ export default {
         'unicorn/filename-case' : [ 'error', {
             'cases' : {
                 'kebabCase'  : true,
-                'pascalCase' : true
+                'pascalCase' : true,
+                'camelCase'  : true // for testing camelCase utils
             }
         } ],
 
